a7734d6224422e76111cf20d1be16470d8aabe93
fbwls <fbwls0218@gmail.com>
2026-01-16 00:17:04 +0900

    Update Blog
    Update Blog

--- site.config.js
+++ site.config.js
@@ -1,32 +1,32 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "Ryu Seo Jin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    name: "류서진",
+    image: "/RyuSeoJin_Black.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "레벨 디자이너",
+    bio: "동선과 시선 유도, 플로우를 고려한 공간 디자인을 지향합니다.",
+    email: "fbwls0218@gmail.com",
+    linkedin: "RyuSeoJin",
+    github: "RyuSeoJin",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Ryu Seo Jin`,
+      href: "https://https://github.com/RyuSeoJin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Ryu Seo Jin",
+    description: "오신 것을 환영합니다.",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://ryuseojin.com/",
   since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
